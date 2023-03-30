5b6e7b89a682b533806544ddcc206b9fe50d36da
Alex S <alex@panoptic.onl>
2023-03-30 13:06:33 -0600

    🎨 rewrote structure to be DRY

--- mod.ts
+++ mod.ts
@@ -1,73 +1,69 @@
-import {decode as  b32decode, encode as b32encode} from "https://deno.land/std/encoding/base32.ts";
+import {
+  decode as b32decode,
+  encode as b32encode,
+} from "https://deno.land/std/encoding/base32.ts";
 
-export async function checkToken(secret: string, token: string | number): Promise<boolean> {
-  const tm = Math.floor(Date.now() / 30000); // Timestamp divided by 30 seconds to get time step
-  const key = await crypto.subtle.importKey(
-    "raw",
-    b32decode(secret),
-    { name: "HMAC", hash: { name: "SHA-1" } },
-    false,
-    ["sign"],
-  ); // convert the secret key to a SHA-1 HMAC hash
-
-  //Create 8-byte buffer for timestamp and OTP
-  const buffer = new ArrayBuffer(8);
-  const view = new DataView(buffer);
-
-  // generate OTPs for current, previous, and following 2 time steps
-  // tolerance is for potential clock skew
-  // adjust ix to generate different time steps
-  for (let ix = -2; ix <= 2; ix++) {
-    const i = (tm + ix) & 0xffffffff;
-    view.setUint32(4, i); // Set timestamp in buffer
-    // sign buffer with key to generate OTP
-    const hm = await crypto.subtle.sign(
-      "HMAC",
-      key,
-      buffer,
-    );
-    // extract 4-byte code from HMAC
-    const bytes = new Uint8Array(hm);
-    const offset = bytes[bytes.length - 1] & 0xf;
-    const code = ((bytes[offset] & 0x7f) << 24) |
-      ((bytes[offset + 1] & 0xff) << 16) |
-      ((bytes[offset + 2] & 0xff) << 8) |
-      (bytes[offset + 3] & 0xff);
-    // truncate code and compare
-    const truncatedCode = code % 1000000;
-    if (token.toString() === truncatedCode.toString()) {
+// check if TOTP token is valid
+// returns boolean
+export async function chkToken(
+  secret: string,
+  token: string | number,
+): Promise<boolean> {
+  const key = await getKey(secret);
+  // Tolerance checks multiple keys to account for clock skew
+  const tolerance = 2;
+  // Timestamp divided by 30 seconds to get time step
+  const currentTime = Math.floor(Date.now() / 30000);
+  for (let i = -tolerance; i <= tolerance; i++) {
+    const timestamp = (currentTime + i) & 0xffffffff;
+    const generatedToken = await genToken(key, timestamp);
+    if (token.toString() === generatedToken.toString()) {
       return true;
     }
   }
   return false;
 }
 
-export async function generateToken(secret: string): Promise<number> {
-  const tm = Math.floor(Date.now() / 30000); // Divide the current Unix timestamp by 30
-  const key = await crypto.subtle.importKey(
-    "raw",
-    b32decode(secret),
-    { name: "HMAC", hash: { name: "SHA-1" } },
-    false,
-    ["sign"],
-  );
+export async function genToken(
+  key: CryptoKey | string,
+  timestamp: number,
+): Promise<number> {
+  //If inputted with a secret string, convert to HMAC
+  if (typeof (key) == "string") {
+    key = await getKey(key);
+  }
+  //Create 8-byte buffer for timestamp and OTP
   const buffer = new ArrayBuffer(8);
   const view = new DataView(buffer);
-  view.setUint32(4, tm);
-
+  // Set timestamp in buffer
+  view.setUint32(4, timestamp);
+  // sign buffer with key to generate OTP
   const hm = await crypto.subtle.sign("HMAC", key, buffer);
+  // extract 4-byte code from HMAC
   const bytes = new Uint8Array(hm);
   const offset = bytes[bytes.length - 1] & 0xf;
   const code = ((bytes[offset] & 0x7f) << 24) |
     ((bytes[offset + 1] & 0xff) << 16) |
     ((bytes[offset + 2] & 0xff) << 8) |
     (bytes[offset + 3] & 0xff);
+  // truncate code 
   const truncatedCode = code % 1000000;
   return truncatedCode;
 }
 
-export function generateSecret(length = 20): string {
+// convert the secret key to a SHA-1 HMAC hash
+async function getKey(secret: string): Promise<CryptoKey> {
+  return await crypto.subtle.importKey(
+    "raw",
+    b32decode(secret),
+    { name: "HMAC", hash: { name: "SHA-1" } },
+    false,
+    ["sign"],
+  );
+}
+
+export function genSecret(length = 20): string {
   const buffer = crypto.getRandomValues(new Uint8Array(length));
   const encoded = b32encode(buffer);
-  return encoded
+  return encoded;
 }
