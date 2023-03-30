59cfac4ef9e2c2be68fb528c10106615f46b32d3
Alex S <alex@panoptic.onl>
2023-03-30 13:06:48 -0600

    test & bench account for new export names

--- mod_bench.ts
+++ mod_bench.ts
@@ -1,13 +1,14 @@
-import { checkToken, generateSecret, generateToken } from "./mod.ts";
+import { chkToken, genSecret, genToken } from "./mod.ts";
 
 Deno.bench(async function benchCheckToken() {
-  await checkToken("7KI7EWAFOZPI4VK23IO6Y7SEV4E55P5Z", "838455")
+  await chkToken("7KI7EWAFOZPI4VK23IO6Y7SEV4E55P5Z", "838455")
 });
 
 Deno.bench(function benchSecret() {
-  generateSecret()
+  genSecret()
 });
 
 Deno.bench(async function benchToken() {
-  await generateToken("7KI7EWAFOZPI4VK23IO6Y7SEV4E55P5Z")
+  const tstamp = 56006746
+  await genToken("7KI7EWAFOZPI4VK23IO6Y7SEV4E55P5Z", 56006746)
 });
--- mod_test.ts
+++ mod_test.ts
@@ -1,20 +1,20 @@
 import { assertEquals, assertExists } from "https://deno.land/std/testing/asserts.ts";
-import { checkToken, generateSecret, generateToken } from "./mod.ts";
+import { chkToken, genSecret, genToken } from "./mod.ts";
 
-Deno.test("generateSecret returns successfully", () => {
-    const secret = generateSecret();
+Deno.test("genSecret returns successfully", () => {
+    const secret = genSecret();
     assertExists(secret)
 })
 
-Deno.test("generateToken returns successfully", () => {
-    const secret = generateSecret();
-    const token = generateToken(secret);
-    assertExists(token)
+Deno.test("genToken returns successfully", async () => {
+    const token = await genToken("7KI7EWAFOZPI4VK23IO6Y7SEV4E55P5Z", 56006746)
+    assertEquals(token, 435356)
 })
 
-Deno.test("checkToken validates OTPs successfully", async () => {
-    const secret = generateSecret();
-    const token = await generateToken(secret);
-    const result = await checkToken(secret, token);
+Deno.test("chckToken validates OTPs successfully", async () => {
+    const secret = genSecret();
+    const tstamp = Math.floor(Date.now() / 30000);
+    const token = await genToken(secret, tstamp)
+    const result = await chkToken(secret, token);
     assertEquals(result, true);
 });
