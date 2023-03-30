0f31f93769d7b04f88b8c86ad24e0d0a99ab465c
Alex S <alex@panoptic.onl>
2023-03-30 13:33:47 -0600

    ✅ Tests account for new export format

--- mod_bench.ts
+++ mod_bench.ts
@@ -1,14 +1,14 @@
-import { chkToken, genSecret, genToken } from "./mod.ts";
+import sstotp from "./mod.ts";
 
 Deno.bench(async function benchCheckToken() {
-  await chkToken("7KI7EWAFOZPI4VK23IO6Y7SEV4E55P5Z", "838455")
+  await sstotp.chkToken("7KI7EWAFOZPI4VK23IO6Y7SEV4E55P5Z", "838455")
 });
 
 Deno.bench(function benchSecret() {
-  genSecret()
+  sstotp.genSecret()
 });
 
 Deno.bench(async function benchToken() {
   const tstamp = 56006746
-  await genToken("7KI7EWAFOZPI4VK23IO6Y7SEV4E55P5Z", 56006746)
+  await sstotp.genToken("7KI7EWAFOZPI4VK23IO6Y7SEV4E55P5Z", 56006746)
 });
--- mod_test.ts
+++ mod_test.ts
@@ -1,20 +1,20 @@
 import { assertEquals, assertExists } from "https://deno.land/std/testing/asserts.ts";
-import { chkToken, genSecret, genToken } from "./mod.ts";
+import sstotp from "./mod.ts";
 
 Deno.test("genSecret returns successfully", () => {
-    const secret = genSecret();
+    const secret = sstotp.genSecret();
     assertExists(secret)
 })
 
 Deno.test("genToken returns successfully", async () => {
-    const token = await genToken("7KI7EWAFOZPI4VK23IO6Y7SEV4E55P5Z", 56006746)
+    const token = await sstotp.genToken("7KI7EWAFOZPI4VK23IO6Y7SEV4E55P5Z", 56006746)
     assertEquals(token, 435356)
 })
 
 Deno.test("chckToken validates OTPs successfully", async () => {
-    const secret = genSecret();
+    const secret = sstotp.genSecret();
     const tstamp = Math.floor(Date.now() / 30000);
-    const token = await genToken(secret, tstamp)
-    const result = await chkToken(secret, token);
+    const token = await sstotp.genToken(secret, tstamp)
+    const result = await sstotp.chkToken(secret, token);
     assertEquals(result, true);
 });
